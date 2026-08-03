73e617aa291f3e7be13c2287b9bfa09334b1b2ff
Dhanishta <your_email@gmail.com>
2026-08-04 04:47:26 +0530

    Add dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
     try {
 
       const response = await axios.post(
-        "http://127.0.0.1:5000/predict",
+        "https://audience-api.koyeb.app/predict",
         {
           ...user,
           Age:Number(user.Age),
